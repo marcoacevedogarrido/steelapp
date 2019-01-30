a545ec8d1427ad7c281e9d3a639a9e75cea1ff23
MarcoAcevedoG <marco160195940@gmail.com>
2019-01-30 09:38:01 -0300

    30/01

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ from app.models import User
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Recordarme')
+    remember_me = BooleanField('Recordar')
     submit = SubmitField('Entrar')
 
 
