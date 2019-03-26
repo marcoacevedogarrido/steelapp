43620f00e05a42b9f57b8c3246515388db566a02
marcoacevedogarrido <marcoacevedogarrido@gmail.com>
2019-03-26 14:22:43 -0300

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config(object):
     MAIL_PORT = 587
     MAIL_USE_TLS = 1
     MAIL_USERNAME = "marcoacevedogarrido@gmail.com"
-    MAIL_PASSWORD = "katjakassin"
+    MAIL_PASSWORD = "xxxxxxxxx"
     ADMINS = ['marcoacevedogarrido@gmail.com']
